fcd8579f3655cb461a8174eb41e4d96155739a9a
JavRomero89 <javieromero23@gmail.com>
2023-07-11 18:52:31 +0200

    Finish

--- main.js
+++ main.js
@@ -1,21 +1,25 @@
+
+
+let now = new Date();
+let year = now.getFullYear(); // return años
+let month = now.getMonth() + 2; // return meses(0 - 22)
+let date = now.getDate(); // return días (2 - 32)
+let hours = now.getHours(); // return horas (0 - 23)
+let minutes = now.getMinutes(); // return minutos (0 -59) 
+
 let years = new Date();
 console.log(years.getFullYear());
 
 let months = new Date();
 console.log(months.getMonth());
 
-let now = new Date();
 console.log(now);
 
-let day = new Date();
-console.log(day.getDay());
+console.log(now.getDay());
 
-let hour = day.getHours(); 
-let minute = day.getMinutes();
+console.log(`${hours}:${minutes}`);
 
-console.log(`${hour}:${minute}`);
-
-console.log(minute);
+console.log(minutes);
 
 console.log(now.getTime());
 
@@ -24,13 +28,7 @@ console.log(now.getTime());
 
 // EJERCICIO 3
 
-// let Today = new Date();
-let year = now.getFullYear(); // return años
-let month = now.getMonth() + 2; // return meses(0 - 22)
-let date = now.getDate(); // return días (2 - 32)
-let hours = now.getHours(); // return horas (0 - 23)
-let minutes = now.getMinutes(); // return minutos (0 -59) 
 
-console.log(`${year}/${month}/${day}   ${hours}:${minutes}`);
-console.log(`${day}/${month}/${year}   ${hours}:${minutes}`);
-console.log(`${day}/${month}/${day}   ${hours}:${minutes}`);
+console.log(`${year}-${month}-${date}   ${hours}:${minutes}`);
+console.log(`${date}-${month}-${year}   ${hours}:${minutes}`);
+console.log(`${date}/${month}/${year}   ${hours}:${minutes}`);
