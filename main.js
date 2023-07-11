c4dd45f8940dcf850ed9d6aadf6a8ddfb00f0dab
JavRomero89 <javieromero23@gmail.com>
2023-07-11 18:32:41 +0200

    advance

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-let year = new Date();
-console.log(year.getFullYear());
+let years = new Date();
+console.log(years.getFullYear());
 
-let month = new Date();
-console.log(month.getMonth());
+let months = new Date();
+console.log(months.getMonth());
 
 let now = new Date();
 console.log(now);
@@ -10,9 +10,27 @@ console.log(now);
 let day = new Date();
 console.log(day.getDay());
 
-let hours = day.getHours(); 
-let minutes = day.getMinutes();
+let hour = day.getHours(); 
+let minute = day.getMinutes();
 
-console.log(`${hours}:${minutes}`);
+console.log(`${hour}:${minute}`);
 
-console.log();
+console.log(minute);
+
+console.log(now.getTime());
+
+// EJERCICIO 2
+
+
+// EJERCICIO 3
+
+// let Today = new Date();
+let year = now.getFullYear(); // return años
+let month = now.getMonth() + 2; // return meses(0 - 22)
+let date = now.getDate(); // return días (2 - 32)
+let hours = now.getHours(); // return horas (0 - 23)
+let minutes = now.getMinutes(); // return minutos (0 -59) 
+
+console.log(`${year}/${month}/${day}   ${hours}:${minutes}`);
+console.log(`${day}/${month}/${year}   ${hours}:${minutes}`);
+console.log(`${day}/${month}/${day}   ${hours}:${minutes}`);
