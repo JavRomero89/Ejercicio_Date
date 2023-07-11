967ed79247af1d56689a912babc01d37bb965b8c
JavRomero89 <javieromero23@gmail.com>
2023-07-11 18:18:28 +0200

    advance

---
+++ main.js
@@ -0,0 +1,18 @@
+let year = new Date();
+console.log(year.getFullYear());
+
+let month = new Date();
+console.log(month.getMonth());
+
+let now = new Date();
+console.log(now);
+
+let day = new Date();
+console.log(day.getDay());
+
+let hours = day.getHours(); 
+let minutes = day.getMinutes();
+
+console.log(`${hours}:${minutes}`);
+
+console.log();
